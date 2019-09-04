b1225441441e1d669e299d3509246c32c95e1a89
JackY <jack.yujia.yuan@gmail.com>
2019-09-04 17:54:40 +1000

    Add axios requests and local error handler.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,21 +7,63 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+  state = {
+    posts: [],
+    seletedPostId: null,
+    error: false,
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        const posts = response.data.splice(0, 3);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Jack Yuan"
+          }
+        })
+        this.setState( {posts: updatedPosts})
+      })
+      .catch(err => {
+        this.setState({error: true})
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({seletedPostId: id});
+  }
+
+  render () {
+    var posts = <p style={{textAlign: 'center'}}>Something went wrong...</p>
+    if (!this.state.error) {
+       posts = this.state.posts.map(post => {
+        return <Post
+          key={post.id}
+          title = {post.title}
+          author = {post.author}
+          onClicked = {() => {this.postSelectedHandler(post.id)}}
+        />
+      })
+    }
+
+
+    return (
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+          <section>
+            <FullPost
+              id = {this.state.seletedPostId}
+              // title = {this.state.posts[this.state.seletedPostId].title}
+              // author = {this.state.posts[this.state.seletedPostId].author}
+            />
+          </section>
+          <section>
+            <NewPost />
+          </section>
+      </div>
         );
     }
 }
