e3cd20acd4730ffbe374bd9c57faae07260bed9c
JackY <jack.yujia.yuan@gmail.com>
2019-09-05 11:48:05 +1000

    Add axios global config and instance.

---
+++ src/axios.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = "INSTANCE AUTH TOKEN";
+
+instance.interceptors.request.use(request => {
+  console.log('Here is the instance request:');
+  console.log(request);
+  return request;
+}, error => {
+  console.log('Here is the request error:');
+  console.log(error);
+  return Promise.reject(error);
+});
+
+export default instance;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'INDEX AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+  console.log('Here is the index request:');
+  console.log(request);
+  return request;
+}, error => {
+  console.log('Here is the index request error:');
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  // console.log(response);
+  return response;
+}, error => {
+  console.log('Here is the response error:');
+  console.log(error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
